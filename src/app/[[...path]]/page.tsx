725365279243795cc9fe704e12efbd247e2e52fc
Minesh Shah <45593010+MineshS@users.noreply.github.com>
2024-07-10 13:57:09 +0100

    first commit

--- src/app/[[...path]]/page.tsx
+++ src/app/[[...path]]/page.tsx
@@ -14,9 +14,7 @@ const { CmsPage:OptimizelyPage, generateMetadata, generateStaticParams } = CmsPa
     }
 })
 
-export const dynamic = "force-static"; // Make sure we cache pages
-export const dynamicParams = true; // Allow new pages to be resolved without rebuilding the site
-export const revalidate = false; // Keep the cache untill manually revalidated using the Webhook
-export const fetchCache = "default-cache"; // Cache fetch results by default
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 export { generateStaticParams, generateMetadata };
 export default OptimizelyPage;
